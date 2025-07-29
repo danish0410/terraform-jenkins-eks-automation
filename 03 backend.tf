21ad262024b5ed107147437c1a754a2dc79fa827
danish0410 <thanigai_velan@yahoo.com>
2025-07-29 10:52:59 +0530

    Update and rename 03 backend.tf to backend.tf

--- 03 backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tf-backup-aws"
-    key            = "dev/terraform.tfstate" # overridden by -backend-config
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
